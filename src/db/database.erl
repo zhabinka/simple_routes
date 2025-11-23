4798d3b5de4b230b6a6939f8cd344dff35b44ae8
Sergey Karpuk <sergeykarpuk@yandex.by>
2025-11-23 12:31:55 +0300

    db connection

--- src/db/database.erl
+++ src/db/database.erl
@@ -1,21 +1,45 @@
--module(db).
--export([])
+-module(database).
+-export([init/0, execute/1, execute/2, query/1, query/2, get_connection/0]).
 
 init() ->
-  {ok, _} = application.ensure_all_started(epgsql),
+  {ok, _} = application:ensure_all_started(epgsql),
   ok.
-
 
 get_connection() ->
   {ok, DbConfig} = application:get_env(simple_routes, db),
-  Host = proplist:get_value(host, DbConfig),
-  Port = proplist:get_value(port, DbConfig),
-  Username = proplist:get_value(username, DbConfig),
-  Password = proplist:get_value(password, DbConfig),
-  Database = proplist:get_value(database, DbConfig),
+  Host = proplists:get_value(host, DbConfig),
+  Port = proplists:get_value(port, DbConfig),
+  Username = proplists:get_value(username, DbConfig),
+  Password = proplists:get_value(password, DbConfig),
+  Database = proplists:get_value(database, DbConfig),
 
-  {ok, Conn} = epgsql:connect(Host, Username, Password, [
-                                                         {database, Database},
-                                                         {port, Port},
-                                                        ]).
+  epgsql:connect(Host,
+                 Username,
+                 Password,
+                 [{database, Database},{port, Port}]).
+
+execute(SQL) ->
+  execute(SQL, []).
+
+execute(SQL, Params) ->
+  {ok, Conn} = get_connection(),
+  try
+    Result = epgsql:equery(Conn, SQL, Params),
+    epgsql:close(Conn),
+    Result
+  catch
+    _:Error ->
+        epgsql:close(Conn),
+        {error, Error}
+  end.
+
+query(SQL) ->
+  query(SQL, []).
+
+query(SQL, Params) ->
+  case execute(SQL, Params) of
+    {ok, _Columns, Rows} -> {ok, Rows};
+    {ok, _Count} -> {ok, []};
+    Error -> Error
+  end.
 
